26a8656696dd88c81db632001088ca6830ed0ec2
sgvili <sagi.gvili@intel.com>
2023-04-09 21:18:54 +0300

    Added gevent package for production usage

--- BE/flask_test.py
+++ BE/flask_test.py
@@ -4,7 +4,8 @@ import json
 import flask
 from flask_cors import CORS
 from flask_sock import Sock
-
+from gevent.pywsgi import WSGIServer
+from gevent import monkey
 
 app = flask.Flask(__name__)
 sock = Sock(app)
@@ -22,6 +23,11 @@ def heavy_func():
         next = str(int(value["Count"]) + 1)
         value["Count"] = next
 
+@app.route('/count')
+def print_count():
+    global value
+    return value
+
 
 @sock.route('/echo')
 def echo(sock):
@@ -32,7 +38,11 @@ def echo(sock):
 
 
 if __name__ == '__main__':
+    monkey.patch_all()
     thread = threading.Thread(target=heavy_func)
     thread.daemon = True
     thread.start()
-    app.run()
+    #app.run() Used without waitress for dev mode
+    http_server = WSGIServer(('127.0.0.1', 5000), app) # keyfile='server.key', certfile='server.crt' attributes for HTTPS
+    print("Prod Server Initiated")
+    http_server.serve_forever()
